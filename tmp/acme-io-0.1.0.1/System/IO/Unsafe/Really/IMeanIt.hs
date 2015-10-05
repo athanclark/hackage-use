2ed8235d4a63fe9ca280cdba9d3cd8a1d1357d4f
Athan Clark <athan.clark@gmail.com>
2015-10-05 11:55:09 -0600

    tmp dirs

--- tmp/acme-io-0.1.0.1/System/IO/Unsafe/Really/IMeanIt.hs
+++
@@ -1,5 +0,0 @@
-module System.IO.Unsafe.Really.IMeanIt (reallyReallyAbsurdlyUnsafePerformIOShameOnYou) where
-
-reallyReallyAbsurdlyUnsafePerformIOShameOnYou :: IO a -> a
-reallyReallyAbsurdlyUnsafePerformIOShameOnYou = error "Trying to do IO? How dare you!"
-
